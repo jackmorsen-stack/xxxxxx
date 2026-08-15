74b15aa545e5e37464c986d2848d1547b2b76487
jackmorsen-stack <mandobesho05@gmail.com>
2026-08-14 22:23:07 -0700

    Update and rename sc.ps1 to message.cmd

---
+++ message.cmd
@@ -0,0 +1,2 @@
+@echo off
+mshta vbscript:MsgBox("Hello User",0,"Test")(window.close)
--- sc.ps1
+++
@@ -1,3 +0,0 @@
-# Just show a message box
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show('Hello User', 'Test')
